37b9c8d7273ef339d91b83a709b979ca246e45f0
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-01 00:04:44 -0500

    change: remove the margin from the nav and add it to the top of the section tag to that every new section has consistent spacing without needed to add margin
    create: start working on the featured categories section

---
+++ src/componenets/FeaturedCategories.jsx
@@ -0,0 +1,11 @@
+import "/src/styles/FeaturedCategories.css";
+
+function FeaturedCategories() {
+  return (
+    <div className="featured-categories">
+      <h1>Featured Categories</h1>
+    </div>
+  );
+}
+
+export default FeaturedCategories;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
 import TopItemsDisplay from "../componenets/TopItemsDisplay";
+import FeaturedCategories from "../componenets/FeaturedCategories";
 import "/src/App.css";
 
 function HomePage({ topItems }) {
   return (
     <>
-      <section className="home-page">
-        <TopItemsDisplay topItems={topItems} />
-      </section>
+      <div className="home-page">
+        <section>
+          <TopItemsDisplay topItems={topItems} />
+        </section>
+        <section>
+          <FeaturedCategories />
+        </section>
+      </div>
     </>
   );
 }
