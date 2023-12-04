b74531d5b9fcd4140fb4ff618cee287d3cd5779f
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-04 17:58:23 -0500

    add: finish an ad banner component for circle to be displayed throughout the application

--- src/componenets/CircleAdBanner.jsx
+++ src/componenets/CircleAdBanner.jsx
@@ -3,18 +3,24 @@ import "/src/styles/CircleAdBanner.css";
 function CircleAdBanner() {
   return (
     <div className="circle-ad-banner">
-      <h2>
-        Get access to hundreds of deals,<br></br> plus offers just for you
-      </h2>
-      <h3>
-        You&apos;re invited to join{" "}
-        <div>
-          <span className="circle-text">
-            <img src="/Target_Bullseye-Logo_Red_transparent.png" alt="" />
-            circle
-          </span>
-        </div>
-      </h3>
+      <div className="circle-ad-text">
+        <h2>
+          Get access to hundreds of deals,<br></br> plus offers just for you
+        </h2>
+        <h3>
+          You&apos;re invited to join{" "}
+          <div>
+            <span className="circle-text">
+              <img src="/Target_Bullseye-Logo_Red_transparent.png" alt="" />
+              circle
+            </span>
+          </div>
+        </h3>
+      </div>
+      <div className="circle-ad-right">
+        <div className="rounded-edge"></div>
+        <img src="/trg.png" alt="" />
+      </div>
     </div>
   );
 }
