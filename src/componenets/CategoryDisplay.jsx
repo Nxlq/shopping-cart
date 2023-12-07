42d3b58e88e622e2fbac2c8df133ce23e75d170e
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-07 00:35:12 -0500

    add: style the category page layout to display items more aesthetically
    refactor: remove the header formatter from the categoryDisplay and
    place it in it's own helperfunctions file to be exported and used
    throughout the application for formatting headers

--- src/componenets/CategoryDisplay.jsx
+++ src/componenets/CategoryDisplay.jsx
@@ -1,13 +1,5 @@
 import { Link } from "react-router-dom";
-
-function formatCategoryHeader(categoryName) {
-  const words = categoryName.split(" ");
-  const formattedWords = words.map(
-    (word) => word[0].toUpperCase() + word.slice(1)
-  );
-  const formattedHeader = formattedWords.join(" ");
-  return formattedHeader;
-}
+import { formatCategoryHeader } from "../helperFunctions";
 
 function CategoryDisplay({ category }) {
   const formattedHeader = formatCategoryHeader(category);
--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,19 +1,25 @@
 import { useParams } from "react-router-dom";
 import ItemCard from "../componenets/ItemCard";
+import "/src/styles/CategoryPage.css";
+import { formatCategoryHeader } from "../helperFunctions";
 
 export default function CategoryPage({ productLog }) {
   const { categoryName } = useParams();
-
   const categoriesProducts = productLog[categoryName];
 
   if (!categoriesProducts)
     return <h1>OOOPS ERROR ? @O@ PAGE THAT CATEFORY DOESNTT EXIST</h1>;
 
+  const formattedName = formatCategoryHeader(categoryName);
+
   return (
-    <div className="category-items__container">
-      {categoriesProducts.map((product) => (
-        <ItemCard itemInfo={product} key={product.id} />
-      ))}
+    <div className="category-page">
+      <h1>{formattedName}</h1>
+      <div className="category-items__container">
+        {categoriesProducts.map((product) => (
+          <ItemCard itemInfo={product} key={product.id} />
+        ))}
+      </div>
     </div>
   );
 }
