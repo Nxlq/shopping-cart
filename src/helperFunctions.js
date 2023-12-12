8044e99165ddbca6e4a3d725f75a45495a091ac9
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-11 22:21:18 -0500

    add: create helper function for formatting names of things to be used inside the url as a path name, it replaces the spaces with '-' and replaces forward slashes '/' with '-per-'

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,8 +1,15 @@
 export function formatCategoryHeader(categoryName) {
-    const words = categoryName.split(" ");
-    const formattedWords = words.map(
-      (word) => word[0].toUpperCase() + word.slice(1)
-    );
-    const formattedHeader = formattedWords.join(" ");
-    return formattedHeader;
-  }
+  const words = categoryName.split(" ");
+  const formattedWords = words.map(
+    (word) => word[0].toUpperCase() + word.slice(1)
+  );
+  const formattedHeader = formattedWords.join(" ");
+  return formattedHeader;
+}
+
+export function formatNameForSEO(name) {
+  const removedSlashes = name.replaceAll("/", "-per-");
+  const formattedName = removedSlashes.replaceAll(" ", "-");
+
+  return formattedName;
+}
