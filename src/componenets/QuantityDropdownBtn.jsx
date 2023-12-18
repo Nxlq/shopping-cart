dc87c278ab06c29b231746fe9b58ed9f9cfad2c4
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-17 22:16:38 -0500

    add: quickly finish the design and position of the drop down quantites menu and implement toggling functionality

--- src/componenets/QuantityDropdownBtn.jsx
+++ src/componenets/QuantityDropdownBtn.jsx
@@ -5,15 +5,42 @@ function QuantityDropdownBtn() {
   const [quantity, setQuantity] = useState(1);
   const [isActive, setIsActive] = useState(false);
 
+  function toggleIsActive() {
+    setIsActive(!isActive);
+  }
+
   return (
-    <button className="quantity-btn">
-      Qty <span className="quantity">{quantity}</span>
-      <img
-        className="quantity-dropdown-arrow"
-        src="/arrow-down-simple-svgrepo-com (2).svg"
-        alt=""
-      />
-    </button>
+    <div className="quantity-dropdown__wrapper">
+      <button onClick={toggleIsActive} className="quantity-btn">
+        Qty <span className="quantity">{quantity}</span>
+        <img
+          className="quantity-dropdown-arrow"
+          src="/arrow-down-simple-svgrepo-com (2).svg"
+          alt=""
+        />
+      </button>
+      {isActive && (
+        <div className="quantity-dropdown">
+          <ul className="quantity-selection">
+            <li>
+              <button>1</button>
+            </li>
+            <li>
+              <button>2</button>
+            </li>
+            <li>
+              <button>3</button>
+            </li>
+            <li>
+              <button>4</button>
+            </li>
+            <li>
+              <button>5</button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
