3270c7fcb96b2aa9d065aed8130e498a4c296fd7
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-20 18:18:16 -0500

    add: there is now an image on the empty cart display to finish the styling long term
    add: include the circle and card banners on the empty cart page as well
    for filler content to make the page look more alive

---
+++ src/componenets/CartItem.jsx
@@ -0,0 +1,7 @@
+import "/src/styles/CartItem.css";
+
+function CartItem({ itemInfo }) {
+  return <div className="cart-item">item</div>;
+}
+
+export default CartItem;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,26 +1,41 @@
 import "/src/styles/CartPage.css";
+import CardAdBanner from "../componenets/CardAdBanner";
+import CircleAdBanner from "../componenets/CircleAdBanner";
 
 function CartPage({ cartItems }) {
   const cartSubTotal = cartItems.reduce((sum, cur) => cur.price + sum, 0);
+  const itemCount = cartItems.length;
   console.log(cartSubTotal);
   return (
-    <>
+    <div className="cart-page">
       {cartItems.length > 0 ? (
-        <div className="cart-header">
-          <h1>Cart</h1>
-          <span>${cartSubTotal}</span>
-        </div>
-      ) : (
-        <div className="empty-cart__container">
-          <button className="help-button">Help</button>
-          <div className="empty-cart-content">
-            <h1>Your cart is empty</h1>
-            <span>Have an account? Sign in to see your cart</span>
-            <button>Sign in</button>
+        <div className="populated-cart__container">
+          <div className="cart-header">
+            <h1>Cart</h1>
+            <span>
+              ${cartSubTotal} subtotal • {itemCount} items
+            </span>
           </div>
         </div>
+      ) : (
+        <>
+          <div className="empty-cart__container">
+            <button className="help-button">Help</button>
+            <div className="empty-cart-content">
+              <h1>Your cart is empty</h1>
+              <span>Have an account? Sign in to see your cart</span>
+              <button>Sign in</button>
+              <img
+                src="/4380801cdd3f730465ca6d9375cc8905.png"
+                alt="cute picture of a dog with a shopping bag"
+              ></img>
+            </div>
+          </div>
+          <CardAdBanner />
+          <CircleAdBanner />
+        </>
       )}
-    </>
+    </div>
   );
 }
 
