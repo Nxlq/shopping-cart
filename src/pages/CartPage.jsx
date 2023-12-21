41bedf2d0104281e216d457707cfd90d7423dc54
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-12-20 22:08:51 -0500

    add: create and style initial layout for the populated cart render
    if the cart is populated the cart page currently render the order
    header and checkout summary(<-- this should be refactored into it's own
    component probably)

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,6 +5,7 @@ import CircleAdBanner from "../componenets/CircleAdBanner";
 function CartPage({ cartItems }) {
   const cartSubTotal = cartItems.reduce((sum, cur) => cur.price + sum, 0);
   const itemCount = cartItems.length;
+  const salesTax = 1.08875;
   console.log(cartSubTotal);
   return (
     <div className="cart-page">
@@ -13,8 +14,50 @@ function CartPage({ cartItems }) {
           <div className="cart-header">
             <h1>Cart</h1>
             <span>
-              ${cartSubTotal} subtotal • {itemCount} items
+              ${cartSubTotal} subtotal • {itemCount}{" "}
+              {`${itemCount === 1 ? "item" : "items"}`}
             </span>
+          </div>
+          <div className="cart-checkout">
+            <h2>Order Summary</h2>
+            <div className="order-summary__row">
+              <img src="/a4108f12d497aeb1feca2930cc51e5a6.png" alt="" />
+              <div className="checkout-spans__container">
+                <span>${(cartSubTotal * salesTax).toFixed(2)} total</span>
+                <span className="cart-count">
+                  {itemCount} {`${itemCount === 1 ? "item" : "items"}`}
+                </span>
+              </div>
+            </div>
+            <div className="order-summary__row">
+              <img className="tag-img" src="/tag-svgrepo-com.svg" alt="" />
+              <div className="checkout-spans__container">
+                <span className="extra-pad">3 offers available</span>
+              </div>
+            </div>
+            <div className="order-summary__row">
+              <img
+                className="tag-img-gift"
+                src="/present-svgrepo-com (1).svg"
+                alt=""
+              />
+              <div className="checkout-spans__container">
+                <span className="extra-pad no-bold">
+                  Not eligible for gift message
+                </span>
+              </div>
+            </div>
+            <div className="order-summary__row">
+              <img
+                className="tag-img-gift"
+                src="/coupon-discount-sale-svgrepo-com.svg"
+                alt=""
+              />
+              <div className="checkout-spans__container last">
+                <span className="extra-pad no-bold">Promo code</span>
+              </div>
+            </div>
+            <button className="checkout-btn">Checkout</button>
           </div>
         </div>
       ) : (
