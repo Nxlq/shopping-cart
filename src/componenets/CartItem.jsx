c36895ceca1e5c4568429d4c2e6e38cc5342e6c1
JeffW <116536323+Nxlq@users.noreply.github.com>
2024-01-09 13:49:14 -0500

    fix: the populated cart page is now mobile responsive

--- src/componenets/CartItem.jsx
+++ src/componenets/CartItem.jsx
@@ -5,29 +5,31 @@ function CartItem({ itemInfo, removeFromCart, changeItemQuantityInCart }) {
   return (
     <div className="cart-item">
       <img src={itemInfo.image} alt="" />
-      <div className="info-container">
-        <span>{itemInfo.title}</span>
-        <div className="btns__container">
-          <QuantityDropdownBtn
-            quantity={itemInfo.quantity}
-            setQuantity={changeItemQuantityInCart}
-            itemId={itemInfo.id}
-          />
-          <button>Save for later</button>
+      <div className="info-flex__wrapper">
+        <div className="info-container">
+          <span>{itemInfo.title}</span>
+          <div className="btns__container">
+            <QuantityDropdownBtn
+              quantity={itemInfo.quantity}
+              setQuantity={changeItemQuantityInCart}
+              itemId={itemInfo.id}
+            />
+            <button>Save for later</button>
+          </div>
         </div>
+        <div className="price__container">
+          <span>${(itemInfo.price * itemInfo.quantity).toFixed(2)}</span>
+          {itemInfo.quantity > 1 && (
+            <span>each ${itemInfo.price.toFixed(2)}</span>
+          )}
+        </div>
+        <button
+          className="remove-btn"
+          onClick={() => removeFromCart(itemInfo.id)}
+        >
+          Remove
+        </button>
       </div>
-      <div className="price__container">
-        <span>${(itemInfo.price * itemInfo.quantity).toFixed(2)}</span>
-        {itemInfo.quantity > 1 && (
-          <span>each ${itemInfo.price.toFixed(2)}</span>
-        )}
-      </div>
-      <button
-        className="remove-btn"
-        onClick={() => removeFromCart(itemInfo.id)}
-      >
-        Remove
-      </button>
     </div>
   );
 }
